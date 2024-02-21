71bc12c31466855ff68c2cbd558814bfc82647f1
mave-wrld <jacob.quaye@stu.ucc.edu.gh>
2024-02-21 14:21:16 -0800

    Added the Remove from wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,5 +5,6 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', views.wishlist, name='wishlist'),
-    path('add_wishlist/<int:hostel_id>/', views.add_wishlist, name='add_wishlist')
+    path('add_wishlist/<int:hostel_id>/', views.add_wishlist, name='add_wishlist'),
+    path('remove_wishlist_item/<int:hostel_id>/', views.remove_wishist_item, name='remove_wishlist_item')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from hostels.models import Hostel
 from .models import Wishlist, WishListItems
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def _wishlist_id(request):
     wishlist = request.session.session_key
     if not wishlist:
-         request.session.create()
+        request.session.create()
         wishlist = request.session.session_key
     return wishlist
 
@@ -22,6 +23,19 @@ def add_wishlist(request, hostel_id):
 
     return redirect('wishlist:wishlist')
 
-def wishlist(request):
-    return render(request, 'wishlist/wishlist.html')
+def remove_wishist_item(request, hostel_id):
+    wishlist = Wishlist.objects.get(wishlist_id=_wishlist_id(request))
+    hostel = get_object_or_404(Hostel, id=hostel_id)
+    wishlist_items = WishListItems.objects.get(hostels=hostel, wishlist=wishlist)
+    wishlist_items.delete()
+    return redirect('wishlist:wishlist')
+
+def wishlist(request, wishlist_items=None):
+    try:
+        wishlist = Wishlist.objects.get(wishlist_id=_wishlist_id(request))
+        wishlist_items = WishListItems.objects.filter(wishlist=wishlist, is_active=True)
+    except ObjectDoesNotExist:
+        pass
+
+    return render(request, 'wishlist/wishlist.html', {'wishlist_items':wishlist_items})
 # Create your views here.
